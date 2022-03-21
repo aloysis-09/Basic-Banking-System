4c753fa43d5d30205ab0cb19513989938c97451e
Aloysis  francis <101318889+aloysis-09@users.noreply.github.com>
2022-03-21 19:26:38 +0530

    Add files via upload

---
+++ PHP/navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : #2b474d;"><b>Spark BANK</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #2b474d;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="../Banking/createuser.php" style="color : #2b474d;"><b>Create User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="../Banking/transfermoney.php" style="color : #2b474d; "><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="../Banking/transactionhistory.php" style="color : #2b474d; margin-right:500px"><b>Transaction History</b></a>
+              </li>
+          </div>
+       </nav>
